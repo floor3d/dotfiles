86a41b281c0ba69b6dd6ca77530ee90f66dfd552
Evan Defloor <evandefloor@yahoo.com>
2023-10-23 15:25:14 -0400

    update ascii location and fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 alias ls 'exa'
 alias v 'vi'
+alias neofetch '~/.config/neofetch/switch-ascii-neofetch; /usr/bin/neofetch'
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    ~/.config/neofetch/switch-ascii-neofetch
 end
