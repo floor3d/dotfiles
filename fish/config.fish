a0896632d7005f106033cb96be198ea788572844
Evan Defloor <evandefloor@yahoo.com>
2023-11-04 20:17:43 -0400

    modify fish config

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,7 @@ alias v 'nvim'
 alias neofetch '~/.config/neofetch/switch-ascii-neofetch; /usr/bin/neofetch'
 # "automatically copy over the terminfo files and also magically enable shell integration on the remote machine."
 alias s 'kitten ssh'
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
