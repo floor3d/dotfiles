2b12ee2468e3c3248b674232eb24f6b9ee83e8a0
Evan Defloor <evandefloor@yahoo.com>
2024-03-13 23:04:37 -0400

    added new wallpapers

--- fish/config.fish
+++ fish/config.fish
@@ -10,9 +10,22 @@ alias neofetch '~/.config/neofetch/switch-ascii-neofetch; /usr/bin/neofetch'
 alias s 'kitten ssh'
 #kubectl facepalm
 #makes kubectl work locally
-alias kubectl="minikube kubectl --"
+alias kubectl "minikube kubectl --"
 
+alias wmlab "xfreerdp ~/Programming/Malware\ Topics\ Test\ Spring\ 2024.rdp /p:Example_Password1 /dynamic-resolution"
+
+alias vpnup "sudo wg-quick up XPS-13"
+
+alias vpndown "sudo wg-quick down XPS-13"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # init zoxide
+    zoxide init --cmd cd fish | source
 end
+
+# Created by `pipx` on 2024-03-12 22:46:21
+set PATH $PATH /home/evan/.local/bin
+# add cargo bin 2 path
+set -gx PATH $PATH /home/evan/.cargo/bin/
+
