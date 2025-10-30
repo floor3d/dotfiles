4341ea2be1257d9ad8ea33ff5776375957ba0534
Evan Defloor <evan@ethicalhack.ing>
2025-10-30 19:09:24 -0400

    add leap keybinds to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,5 @@
 vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next Buffer' })
 vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Previous Buffer' })
 vim.keymap.set('n', '-', 'yiw/\\v<C-R>0\\([^)]*(\\)|$)<CR>N', {desc = 'Find all instances of function calls or declarations with word under cursor'})
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
