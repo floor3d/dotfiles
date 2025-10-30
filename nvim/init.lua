68516c541f573180331a1173d2961d185ed0bca5
Evan Defloor <evan@ethicalhack.ing>
2025-10-29 20:46:16 -0400

    add keymappings to add to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+-- Custom keymaps to add to init.lua.
+vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Previous Buffer' })
+vim.keymap.set('n', '-', 'yiw/\\v<C-R>0\\([^)]*(\\)|$)<CR>N', {desc = 'Find all instances of function calls or declarations with word under cursor'})
